0256d8bd61c4385ce0bd8fd30690cbdd05008e37
mclacore <michael@massdriver.cloud>
2023-12-19 16:28:37 -0700

    Adding org ID support

--- src/mass/bundleBuild.ts
+++ src/mass/bundleBuild.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 import { getToken } from '../settings';
+import { getOrgId } from '../settings';
 import { exec } from 'child_process';
 
 function bundleBuild() {
@@ -11,27 +12,32 @@ function bundleBuild() {
   }
 
   const token = getToken();
+  const orgId = getOrgId();
   const currentDir = path.dirname(editor.document.uri.fsPath);
 
   if (token) {
-    if (fs.existsSync(currentDir + '/massdriver.yaml')) {
-      exec(`export MASSDRIVER_API_KEY=${token} && cd ${currentDir} && mass bundle build`, (err: Error | null) => {
-        if (err) {
-          console.error(`exec error: ${err.message}`);
-        } else {
-          vscode.window.showInformationMessage('Bundle built successfully');
-        }
-      });
-    } else if (fs.existsSync(currentDir + '/../massdriver.yaml')) {
-      exec(`cd ${currentDir} && cd .. && export MASSDRIVER_API_KEY=${token} && mass bundle build`, (err: Error | null) => {
-        if (err) {
-          console.error(`exec error: ${err.message}`);
-        } else {
-          vscode.window.showInformationMessage('Bundle built successfully');
-        }
-      });
+    if (orgId) {
+      if (fs.existsSync(currentDir + '/massdriver.yaml')) {
+        exec(`export MASSDRIVER_API_KEY=${token} && export MASSDRIVER_ORG_ID=${orgId} && cd ${currentDir} && mass bundle build`, (err: Error | null) => {
+          if (err) {
+            console.error(`exec error: ${err.message}`);
+          } else {
+            vscode.window.showInformationMessage('Bundle built successfully');
+          }
+        });
+      } else if (fs.existsSync(currentDir + '/../massdriver.yaml')) {
+        exec(`cd ${currentDir} && cd .. && export MASSDRIVER_API_KEY=${token} && export MASSDRIVER_ORG_ID=${orgId} && mass bundle build`, (err: Error | null) => {
+          if (err) {
+            console.error(`exec error: ${err.message}`);
+          } else {
+            vscode.window.showInformationMessage('Bundle built successfully');
+          }
+        });
+      } else {
+        vscode.window.showErrorMessage('massdriver.yaml not found in the current directory');
+      }
     } else {
-      vscode.window.showErrorMessage('massdriver.yaml not found in the current directory');
+      vscode.window.showErrorMessage('No organization ID found in settings.');
     }
   }
 }
